11d9f8a023c7b18d2dfc6f98b9a069e48728b567
CxrlosKenobi <cxrlos@kenobi.dev>
2026-03-16 16:18:49 -0300

    Initialize project with Next.js, TypeScript, and Tailwind CSS setup, including ESLint configuration, global styles, and a basic layout. Added .gitignore and yarn.lock for dependency management.

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#ffffff",
+        color: "#000000",
+      }}
+    >
+      <span>Hello world</span>
+    </div>
+  );
+}
