37cfc61a24abc5d8ad4b6e1e6d719dcaf6fc8c09
CxrlosKenobi <cxrlos@kenobi.dev>
2026-03-20 11:38:03 -0300

    fix: Corrected "Iniciar sesion" typo to "Iniciar sesión" and adjusted Navbar button display.

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,6 @@ export default function RootLayout({
         <ClerkProvider localization={esES}>
           <Providers>{children}</Providers>
         </ClerkProvider>
-
       </body>
     </html>
   );
